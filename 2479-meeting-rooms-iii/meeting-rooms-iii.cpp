7465f0455e2b45cc0b029d992954da6f0288b418
Ruved Aalam <ruvedalam7776@gmail.com>
2024-05-21 16:08:19 +0530

    Time: 342 ms (36.59%) | Memory: 110.6 MB (25.95%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,20 +1,58 @@
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>>& meets) {
-       int cnt[101] = {};
-    sort(begin(meets), end(meets));
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-    for (int i = 0; i < n; ++i)
-        pq.push({meets[0][0], i});
-    for (auto &m : meets) {
-        while (pq.top().first < m[0]) {
-            pq.push({m[0], pq.top().second});
-            pq.pop();
-        }
-        auto [start, room] = pq.top(); pq.pop();
-        pq.push({start + m[1] - m[0], room});
-        ++cnt[room];
+bool static compare(vector<int>& v1, vector<int>& v2) {
+    return v1[0] < v2[0];
+}
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+    sort(meetings.begin(), meetings.end(), compare);
+        
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> engaged;
+        
+    
+    priority_queue<int, vector<int>, greater<int>> unused;
+        
+    unordered_map<int, int> freq;
+        
+        /* Currently all the rooms are mepty */
+    for(int i = 0; i < n; i++) {
+        unused.push(i);
     }
-    return max_element(begin(cnt), end(cnt)) - begin(cnt);
+        
+    for(auto x:meetings) {
+        int s = x[0], e = x[1];
+        
+        while(engaged.size() > 0 && engaged.top().first <= s) {
+            int room = engaged.top().second;
+            engaged.pop();
+            unused.push(room);
+        }
+        
+        if(unused.size() > 0) 
+        {
+            int room = unused.top();
+            unused.pop(); 
+            freq[abs(room)] += 1;
+            engaged.push({e, room});
+        }
+        else 
+        {
+            pair<long long, int> topmost = engaged.top();
+            engaged.pop();
+        
+            freq[abs(topmost.second)] += 1;
+            long long newEnd = topmost.first;
+            newEnd += (e - s);
+            engaged.push({newEnd, topmost.second});
+        }
+    }
+
+    int maxi = 0;
+    for(int i = 1; i < n; i++) {
+        if(freq[i] > freq[maxi])
+            maxi = i;
+    }
+
+    return maxi;
+
     }
 };
