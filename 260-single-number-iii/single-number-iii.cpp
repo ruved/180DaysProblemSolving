d1c0425e69695bdd6d5ff00ce917cc402da3102a
Ruved Aalam <ruvedalam7776@gmail.com>
2024-05-31 21:39:19 +0530

    Time: 5 ms (79.18%) | Memory: 12.3 MB (64.77%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int overall_xor = 0;
+        for (int n : nums) {
+            overall_xor ^= n;
+        }
+
+        int first_group_xor = 0;
+        int second_group_xor = 0;
+
+        int bit_pos_dif = 0;
+        while (((overall_xor >> bit_pos_dif) & 1) != 1) {
+            bit_pos_dif++;
+        }
+
+        for (int num : nums) {
+            if (((num >> bit_pos_dif) & 1) == 1) {
+                first_group_xor ^= num;
+            } else {
+                second_group_xor ^= num;
+            }
+        }
+
+        return {first_group_xor, second_group_xor};
+    } 
+    
+};
