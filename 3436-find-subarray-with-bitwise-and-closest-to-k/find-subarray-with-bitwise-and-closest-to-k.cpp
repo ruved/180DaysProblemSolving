af8efc00ab98b85ca0e0c7e7281668b4ae209ed8
Ruved Aalam <ruvedalam7776@gmail.com>
2024-06-03 03:36:16 +0530

    Time: 141 ms (100.00%) | Memory: 94.7 MB (50.00%) - LeetSync

--- 3436-find-subarray-with-bitwise-and-closest-to-k/find-subarray-with-bitwise-and-closest-to-k.cpp
+++ 3436-find-subarray-with-bitwise-and-closest-to-k/find-subarray-with-bitwise-and-closest-to-k.cpp
@@ -3,15 +3,15 @@ public:
     int minimumDifference(vector<int>& nums, int k) {
         int ans = INT_MAX; 
         for(int i=0; i<nums.size(); i++){
-            int x = nums[i]; // Get the current element
-            ans = min(ans, abs(x-k)); // Update the answer with the minimum difference between the current element and k
+            int x = nums[i]; 
+            ans = min(ans, abs(x-k)); 
 
-            for(int j=i; j<nums.size() && j<i+35; j++){ // Iterate through the next 35 elements or until the end of the array
-                x = (x&nums[j]); // Update x by performing bitwise AND with the next element
-                ans = min(ans, abs(x - k)); // Update the answer with the minimum difference between the updated x and k
+            for(int j=i; j<nums.size() && j<i+35; j++){ 
+                x = (x&nums[j]); 
+                ans = min(ans, abs(x - k)); 
             }
         }
         
-        return ans; // Return the final answer
+        return ans; 
     }
 };
