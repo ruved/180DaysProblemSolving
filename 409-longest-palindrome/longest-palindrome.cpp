1acb6d49f33ef4f7c5cd6415637e44b46a23e26e
Ruved Aalam <ruvedalam7776@gmail.com>
2024-06-04 05:51:44 +0530

    Time: 4 ms (44.24%) | Memory: 7.8 MB (75.88%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-bool static cmp(int &a,int &b){
-    return a>b;
-}
+
     int longestPalindrome(string s) {
         vector<int>mp(256,0);
         for(int i=0;i<s.length();++i)
             mp[s[i]]++;
-        sort(mp.begin(),mp.end(),cmp);
+       
         int ans=0;
         bool odd=false;
         for(int i=0;i<mp.size();++i){
