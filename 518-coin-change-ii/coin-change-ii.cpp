98c43c23b87d87be29a349b8f15a8a787370d411
Ruved Aalam <ruvedalam7776@gmail.com>
2024-06-18 01:34:52 +0530

    Time: 7 ms (70.63%) | Memory: 19.9 MB (31.18%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+
 int help(int amount, vector<int>& coins,int i,vector<vector<int>>&dp){
     if(i<0&&amount!=0) return 0;
     if(i<0&&amount==0) return 1;
@@ -12,7 +14,21 @@ int help(int amount, vector<int>& coins,int i,vector<vector<int>>&dp){
 
 }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-    return  help(amount,coins,coins.size()-1,dp);
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,0));
+        dp[0]={0};
+        dp[0][0]=1;
+        int take=0,notake=0;
+        for(int i=0;i<coins.size();++i){
+            for(int j=0;j<=amount;++j){
+                take=0;
+                if(j>=coins[i]) 
+                 take=dp[i+1][j-coins[i]];//help(amount-coins[i],coins,i,dp);
+                    notake=dp[i][j];//help(amount,coins,i-1,dp);
+                 dp[i+1][j]=take+notake;
+            }
+        }
+        return dp[coins.size()][amount];
+    // return  help(amount,coins,coins.size()-1,dp);
+
     }
 };
