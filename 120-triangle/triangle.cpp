0383504ee0d9e40f9b7335634837c26dc318dac9
Ruved Aalam <ruvedalam7776@gmail.com>
2024-06-26 19:32:49 +0530

    Time: 4 ms (73.18%) | Memory: 11.1 MB (82.67%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,14 +3,17 @@ public:
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int row=triangle.size();
-        vector<vector<int>>dp(row,vector<int>(row,0));
+       vector<int>cur(row,0);
+       vector<int>next(row,0);
         for(int i=0;i<row;++i)
-            dp[row-1][i]=triangle[row-1][i];
+            cur[i]=triangle[row-1][i];
 
-        for(int r=row-2;r>=0;--r)
+        for(int r=row-2;r>=0;--r){
             for(int col=0;col<=r;++col)
-            dp[r][col]=triangle[r][col]+min(dp[r+1][col],dp[r+1][col+1]);  
+                next[col]=triangle[r][col]+min(cur[col],cur[col+1]);
+                cur=next;
+        }  
         
-        return dp[0][0];
+        return cur[0];
     }
 };
