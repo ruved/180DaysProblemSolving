902fcd8976e6525af54417283be13adc1f61fb21
Ruved Aalam <ruvedalam7776@gmail.com>
2024-06-26 19:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (32.70%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-int help(vector<vector<int>>& triangle,int row,int col,vector<vector<int>>&dp){
-    if(row==triangle.size()) return 0;
-    if(dp[row][col]!=-1) return dp[row][col];
-  return   dp[row][col]=triangle[row][col]+min(help(triangle,row+1,col,dp),help(triangle,row+1,col+1,dp));
-}
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int row=triangle.size();
-        vector<vector<int>>dp(row,vector<int>(row,-1));
-      return  help(triangle,0,0,dp);
+        vector<vector<int>>dp(row,vector<int>(row,0));
+        for(int i=0;i<row;++i)
+            dp[row-1][i]=triangle[row-1][i];
+
+        for(int r=row-2;r>=0;--r)
+            for(int col=0;col<=r;++col)
+            dp[r][col]=triangle[r][col]+min(dp[r+1][col],dp[r+1][col+1]);  
+        
+        return dp[0][0];
     }
 };
-
-// 2
-// 3 4
-// 6 5 7
-// 4 1 8 3
