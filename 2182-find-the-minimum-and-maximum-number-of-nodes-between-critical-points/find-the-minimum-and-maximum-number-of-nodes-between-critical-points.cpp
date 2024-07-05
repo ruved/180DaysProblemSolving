db1a271213d67fab7dce9ed1a95e1ddc85447057
Ruved Aalam <ruvedalam7776@gmail.com>
2024-07-05 05:57:11 +0530

    Time: 183 ms (13.35%) | Memory: 119.9 MB (6.90%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -14,25 +14,31 @@ public:
         ListNode *cur=head->next;
         ListNode *prev=head;
         vector<int>ans;
+        int mindis=INT_MAX;
         int count=2;
+        int ansSize=0;
         while(cur->next){
             int x=cur->val;
             int y=prev->val;
             int z=cur->next->val;
-            if((x>y&&x>z)||(x<y&&x<z))
-            ans.push_back(count);
+            if((x>y&&x>z)||(x<y&&x<z)){
+                ans.push_back(count);
+                ansSize++;
+                if(ansSize>=2)
+                 mindis=min(mindis,ans[ansSize-1]-ans[ansSize-2]);
+            }
             prev=cur;
             cur=cur->next;
             count++;
         }
         if(ans.size()<2) return {-1,-1};
-        sort(ans.begin(),ans.end());
+        // sort(ans.begin(),ans.end());
         int maxdis=ans[ans.size()-1]-ans[0];
-        int mindis=INT_MAX;
-        for(int i=1;i<ans.size();++i)
-        {
-            mindis=min(mindis,ans[i]-ans[i-1]);
-        }
+        
+        // for(int i=1;i<ans.size();++i)
+        // {
+           
+        // }
         return {mindis,maxdis};
     }
 };
