4bdbc3f6a3fa5bf39467ace245a638879d0fbd6e
Ruved Aalam <ruvedalam7776@gmail.com>
2024-07-24 19:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (62.48%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    int help(string s,int i,vector<int>&dp)
-    {
-        if(i==s.length())
-            return 1;
-        if(dp[i]!=-1) return dp[i];
-        
-        int ans=0;
-        
-        if(s[i]=='0') return 0;
-    
-        if(i<s.length()-1)
-        {
-            if((s[i]=='1'||s[i]=='2')&&s[i+1]=='0')
-                ans+=help(s,i+2,dp);
-            else
-            {
-                if((s[i]=='2'&&s[i+1]-'0'<=6)||s[i]=='1')
-                    ans+=help(s,i+2,dp);
-            }  
-        }
-        ans+=help(s,i+1,dp);
-        dp[i]=ans;
-         return ans;   
+int help(string &s,int i,vector<int>&dp){
+    if(i>=s.length()) return  1;
+    if(s[i]=='0') return 0;
+    if(dp[i]!=-1) return dp[i];
+    int res=0;
+    if(s[i]=='2'){
+        if(i+1<s.length()&&s[i+1]<='6')
+            res= help(s,i+2,dp);
     }
+    else if(s[i]=='1'&&i+1<s.length())
+        res= help(s,i+2,dp);
+
+        res+=help(s,i+1,dp);
+        return dp[i]=res;
+
+}
     int numDecodings(string s) {
-        vector<int>dp(s.length()+1,-1);
+        vector<int>dp(s.length(),-1);
         return help(s,0,dp);
     }
 };
-           
-//11101
-//s[i]=0
