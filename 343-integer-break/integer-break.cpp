b7bd6c91abcceb57599344d95d3ef1e56df65e07
Ruved Aalam <ruvedalam7776@gmail.com>
2024-07-27 20:04:27 +0530

    Time: 3 ms (36.08%) | Memory: 7.7 MB (12.32%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -9,13 +9,17 @@ int help(int n,vector<int>&dp){
        ans=max(ans,i*help(n-i,dp));
     return dp[n]=ans;
 }
+
     int integerBreak(int n) {
+        if(n<=2) return 1;
+        if(n==3) return 2;
+
         int ans=1;
         vector<int>dp(n+1,-1);
-        for(int i=1;i<n;++i){
-
-           ans=max(ans,i*help(n-i,dp));
-        }
+        // for(int i=1;i<n;++i){
+        //    ans=max(ans,i*help(n-i,dp));
+        // }
+        ans=help(n,dp);
         return ans;
     }
 };
