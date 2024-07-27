d91abaa81487de0a28312b5d229a8d7412101420
Ruved Aalam <ruvedalam7776@gmail.com>
2024-07-27 16:39:46 +0530

    Time: 526 ms (9.16%) | Memory: 222 MB (25.59%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,7 +3,7 @@ public:
 int help(vector<int>& prices,int i,bool buy,int k,vector<vector<vector<int>>>&dp){
     if(i==prices.size()||k==0) return 0;
 
-    if(dp[i][k][buy]!=-1) return dp[i][k][buy];
+    if(dp[i][k-1][buy]!=-1) return dp[i][k-1][buy];
     int take=0,notake=0;
     if(buy)
         take = -prices[i]+help(prices,i+1,false,k,dp);
@@ -11,13 +11,13 @@ int help(vector<int>& prices,int i,bool buy,int k,vector<vector<vector<int>>>&dp
         take = prices[i]+help(prices,i+1,true,k-1,dp);
 
     notake=help(prices,i+1,buy,k,dp);
-    return dp[i][k][buy]=max(take,notake);
+    return dp[i][k-1][buy]=max(take,notake);
     
 }
     int maxProfit(vector<int>& prices) {
          int n=prices.size();
          int k=2;
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(3,vector<int>(2,-1)));
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(k,vector<int>(2,-1)));
         int ans= help(prices,0,true,k,dp);
         return ans<0?0:ans;
     }
