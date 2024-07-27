5e6d25e30a34f852a25be98d2fc87752e6d34819
Ruved Aalam <ruvedalam7776@gmail.com>
2024-07-27 17:49:12 +0530

    Time: 7 ms (72.89%) | Memory: 20.3 MB (18.96%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
+int help(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
+    if(amount==0) return 1;
+    if(i<0) return 0;
 
-
-int help(int amount, vector<int>& coins,int i,vector<vector<int>>&dp){
-    if(i<0&&amount!=0) return 0;
-    if(i<0&&amount==0) return 1;
     if(dp[i][amount]!=-1) return dp[i][amount];
-    int take=0,notake=0;
-    if(amount>=coins[i]) 
-        take=help(amount-coins[i],coins,i,dp);
-    notake=help(amount,coins,i-1,dp);
-    return dp[i][amount]=take+notake;
 
+    int take=0,notake=0;
+    if(amount>=coins[i])
+        take=help(coins,amount-coins[i],i,dp);
+    
+    notake=help(coins,amount,i-1,dp);
+    return dp[i][amount]=take+notake;
 }
     int change(int amount, vector<int>& coins) {
-        // vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,0));
-        vector<int>prev(amount+1,0),cur(amount+1,0);
-        prev[0]=1;
-        
-        int take=0,notake=0;
-        for(int i=0;i<coins.size();++i){
-            for(int j=0;j<=amount;++j){
-                take=0;
-                if(j>=coins[i]) 
-                 take=cur[j-coins[i]];//help(amount-coins[i],coins,i,dp);
-                notake=prev[j];//help(amount,coins,i-1,dp);
-                 cur[j]=take+notake;
-            }
-            prev=cur;
-        }
-        return prev[amount];
-    // return  help(amount,coins,coins.size()-1,dp);
-
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        int ans= help(coins,amount,coins.size()-1,dp);
+        return ans;
     }
 };
