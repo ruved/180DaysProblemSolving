cf5c97a8d117dbcfc47dbf85917dfb84975b40d5
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-01 19:02:02 +0530

    Time: 29 ms (54.25%) | Memory: 16.4 MB (58.83%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
+void help(vector<vector<char>>& grid,int i,int j,vector<vector<bool>>&vis){
+    if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||vis[i][j]) return ;
+    if(grid[i][j]=='0') return ;
+    vis[i][j]=true;
+    help(grid,i-1,j,vis);
+    help(grid,i+1,j,vis);
+    help(grid,i,j-1,vis);
+    help(grid,i,j+1,vis);
     
-    bool isSafe(vector<vector<char>>& grid,int row,int col,vector<vector<bool> >&visited)
-    {
-    return row>=0&&row<visited.size()&&col>=0&&col<visited[0].size()&&grid[row][col]=='1'&&!visited[row][col];
-    }
-  void  DFScount(vector<vector<char>>& grid,int row,int col,vector<vector<bool> >&visited)
-  {
-      int rowbar[]={-1,0,1,0};
-      int colbar[]={0,-1,0,1};
-      visited[row][col]=true;
-      for(int k=0;k<4;++k)
-          if(isSafe(grid,row+rowbar[k],col+colbar[k],visited))
-              DFScount(grid,row+rowbar[k],col+colbar[k],visited);
-      
-  }
+
+}
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.size()==0)
-            return 0;
-        int n=grid.size();
-        int m=grid[0].size();
-        int count=0;
-        vector<vector<bool> >visited(n,vector<bool>(m,0));
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-            if(grid[i][j]=='1'&&!visited[i][j]){
-                DFScount(grid,i,j,visited);
-                count++;
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        int ans=0;
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(!vis[i][j]&&grid[i][j]=='1'){
+                    help(grid,i,j,vis);
+                    ans++;
+                }    
             }
-                return count;
+        }
+        return ans;
     }
 };
