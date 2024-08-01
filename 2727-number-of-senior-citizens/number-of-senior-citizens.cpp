9e98e5a0851faae980c0fa87c7537eb50188f47e
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-01 18:29:39 +0530

    Time: 4 ms (82.75%) | Memory: 17.6 MB (16.71%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans=0;
+        for(int i=0;i<details.size();++i)
+            if((details[i][11]=='6'&&details[i][12]>'0')||(details[i][11]>'6'))
+                ans++;
+        
+        return ans;
+    }
+};
