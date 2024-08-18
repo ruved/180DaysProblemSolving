513013794909fcf6a61517cd59cc7008fb269c8d
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-19 00:10:59 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (61.59%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
 int help(vector<int>& days, vector<int>& costs,int i,vector<int>&dp){
-    if(i>=days.size()) return 0;
+    if(i<0) return 0;
+
     if(dp[i]!=-1) return dp[i];
-    int a=costs[0]+help(days,costs,i+1,dp);
-    int week=days[i]+7-1;
-    int j=i;
-    while(j<days.size()&&week>=days[j]){
-        j++;
+
+    int day1=costs[0]+help(days,costs,i-1,dp);
+    
+    int j=i-1;
+    while(j>=0&&days[j]>days[i]-7){
+        j--;
     }
-    int b=costs[1]+help(days,costs,j,dp);
-    int month=days[i]+30-1;
-    j=i;
-    while(j<days.size()&&month>=days[j]){
-        j++;
+    int day7=costs[1]+help(days,costs,j,dp);
+
+     j=i-1;
+    while(j>=0&&days[j]>days[i]-30){
+        j--;
     }
-    int c=costs[2]+help(days,costs,j,dp);
-    return dp[i]=min(min(a,b),c);
+    int day30=costs[2]+help(days,costs,j,dp);
+    return dp[i]=min(min(day1,day7),day30);
 }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size(),-1);
-      return   help(days,costs,0,dp);
+       int n=days.size();
+       vector<int>dp(n,-1);
+       return  help(days,costs,n-1,dp); 
     }
 };
