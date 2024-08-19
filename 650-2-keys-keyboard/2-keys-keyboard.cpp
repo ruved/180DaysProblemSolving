fdfaee0e31def35f263628e0150bb2d4794f358b
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-20 01:05:03 +0530

    Time: 31 ms (13.21%) | Memory: 157.2 MB (8.56%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-int help(int &n,int copy,int v,vector<vector<int>>&dp)
-{
-    if(v==n) return 0;
-    if(v>n) return INT_MAX;
+int help(int &total,int n,int last,vector<vector<int>>&dp){
+    if(n==total) return 0;
+    if(n>total) return INT_MAX;
 
-    if(dp[v][copy]!=-1) return dp[v][copy];
-    int take=INT_MAX,nottake=INT_MAX;
-    if(v!=copy)
-    nottake=help(n,v,v,dp);
+    if(dp[n][last]!=-1) return dp[n][last];
+    int op1=INT_MAX;
+    if(n!=last)
+    op1=help(total,n,n,dp);
     
-    take=help(n,copy,v+copy,dp);
-    int val=min(take,nottake);
-    return dp[v][copy]=val!=INT_MAX?val+1:val;
+    int op2=help(total,n+last,last,dp);
+    int val=min(op1,op2);
+    return dp[n][last]=(val!=INT_MAX?val+1:val);
 }
     int minSteps(int n) {
-    if(n==1)  return 0;
-    vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-    int copy=1; 
-    return 1+help(n,copy,1,dp); 
+        if(n==1) return 0;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int a=n;
+        return 1+help(n,1,1,dp);
     }
 };
