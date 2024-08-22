9d725aec64b70c1ae55abaac4f5ae21fdb9d814a
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-23 04:13:39 +0530

    Time: 252 ms (15.68%) | Memory: 163.1 MB (7.90%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -4,10 +4,9 @@ int help(vector<vector<int>>& pairs,int prev,int i,vector<vector<int>>&dp){
     if(i>=pairs.size()) return 0;
     if(dp[prev+1][i]!=-1) return dp[prev+1][i];
     int take=0;
-    if(prev==-1)
+    if(prev==-1||pairs[prev][1]<pairs[i][0])
         take=1+help(pairs,i,i+1,dp);
-    else if(pairs[prev][1]<pairs[i][0])
-        take=1+help(pairs,i,i+1,dp);
+        
     int notake=help(pairs,prev,i+1,dp);
     return dp[prev+1][i]=max(take,notake);
 
