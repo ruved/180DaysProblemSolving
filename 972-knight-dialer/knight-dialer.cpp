699a5f80a02e97a3ea767a23acbf54e2196b1917
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-27 21:03:08 +0530

    Time: 69 ms (67.59%) | Memory: 24.3 MB (61.06%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
 int mod=1e9+7;
+vector<vector<int>>numpadKnight={{4,6},{6,8},{7,9},{4,8},{0,3,9},{},{0,1,7},{2,6},{1,3},{2,4}};
+
 int help(int n,int pos,vector<vector<int>>&dp){
     if(n<=0) return 1;
     if(dp[pos][n]!=-1) return dp[pos][n];
     long res=0;
-    switch(pos){
-        case 0:
-            res+=help(n-1,6,dp)+help(n-1,4,dp);
-        break;
-        case 1:
-            res+=help(n-1,6,dp)+help(n-1,8,dp);
-        break;
-        case 2:
-            res+=help(n-1,7,dp)+help(n-1,9,dp);
-        break;
-        case 3:
-            res+=help(n-1,4,dp)+help(n-1,8,dp);
-        break;
-        case 4:
-            res+=help(n-1,3,dp)%mod;
-            res+=help(n-1,9,dp)%mod;
-            res+=help(n-1,0,dp)%mod;
-        break;
-        // case 5:
-        //     res+=help(n-1,6)+help(n-1,8);
-        break;
-        case 6:
-            res+=help(n-1,0,dp)%mod;
-            res+=help(n-1,1,dp)%mod;
-            res+=help(n-1,7,dp)%mod;
-        break;
-        case 7:
-            res+=help(n-1,2,dp)+help(n-1,6,dp);
-        break;
-        case 8:
-            res+=help(n-1,1,dp)+help(n-1,3,dp);
-        break;
-        case 9:
-            res+=help(n-1,4,dp)+help(n-1,2,dp);
-        break;
-    }
+    for(int j=0;j<numpadKnight[pos].size();++j){
+        res+=help(n-1,numpadKnight[pos][j],dp);
+        res=res%mod;
+        }
      return dp[pos][n]=res%=mod;
 }
     int knightDialer(int n) {
