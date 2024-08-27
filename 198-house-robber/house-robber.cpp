62951624dbe977179b9bc064667423be2ee7d0d3
Ruved Aalam <ruvedalam7776@gmail.com>
2024-08-27 18:56:58 +0530

    Time: 4 ms (31.85%) | Memory: 9.6 MB (37.82%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-int help(vector<int>& nums,int i,bool canRob,vector<vector<int>>&dp){
+int help(vector<int>& nums,int i,vector<int>&dp){
     if(i<0) return 0;
-
-    if(dp[i][canRob]!=-1) return dp[i][canRob];
-    int take=0,notake=0;
-    if(canRob){
-        take=nums[i]+help(nums,i-1,false,dp);
-    }
-    notake=help(nums,i-1,true,dp);
-
-    return dp[i][canRob]=max(take,notake);
+    if(dp[i]!=-1) return dp[i];
+    int take=nums[i]+help(nums,i-2,dp);
+    int notake=help(nums,i-1,dp);
+    return dp[i]=max(take,notake);
 }
     int rob(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(2,-1));
-        return  help(nums,nums.size()-1,true,dp);
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return help(nums,n-1,dp);
     }
 };
