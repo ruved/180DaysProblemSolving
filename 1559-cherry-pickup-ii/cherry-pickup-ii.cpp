c0a714a6c1f87810c66537d591326cbaab09705e
Ruved Aalam <ruvedalam7776@gmail.com>
2024-09-03 01:17:34 +0530

    Time: 51 ms (55.34%) | Memory: 19.7 MB (49.95%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
 int help(vector<vector<int>>& grid,int i,int j1,int j2,vector<vector<vector<int>>>&dp){
-
-if(i>=grid.size()||j1<0||j1>=grid[0].size()||j2<0||j2>=grid[0].size()) return 0;
+    
+    if(i>=grid.size()) return 0;
+    if(j1<0||j1>=grid[0].size()||j2<0||j2>=grid[0].size()) return INT_MIN;
 
     if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-    vector<int>ans;
-    
-    for(int k=-1;k<=1;++k)
-        for(int l=-1;l<=1;++l)
-            ans.push_back(help(grid,i+1,j1+k,j2+l,dp));
-    
-    int mx= *max_element(ans.begin(),ans.end());
-    
-   int sum=0;
+    int ans=grid[i][j1];;
     if(j1!=j2)
-        sum=grid[i][j1]+grid[i][j2];
-    else
-    sum= grid[i][j1];
-    return dp[i][j1][j2]=sum+mx;
+        ans+=grid[i][j2];
 
+    int res=0;
+    for(int newcol1=j1-1;newcol1<=j1+1;++newcol1)
+        for(int newcol2=j2-1;newcol2<=j2+1;++newcol2)
+            res=max(res,help(grid,i+1,newcol1,newcol2,dp));
+
+    return dp[i][j1][j2]=(res!=INT_MIN)?res+ans:res;
 }
     int cherryPickup(vector<vector<int>>& grid) {
-        int row=grid.size();
-        int col=grid[0].size();
-        vector<vector<vector<int>>>dp(row,vector<vector<int>>(col,vector<int>(col,-1)));
-        return help(grid,0,0,col-1,dp);
+        int n=grid[0].size();
+        int m=grid.size();
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        return help(grid,0,0,n-1,dp);
     }
 };
