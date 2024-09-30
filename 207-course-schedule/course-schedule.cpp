06c7670f5912695a31fddb1dc79b054c30c8fca7
Ruved Aalam <ruvedalam7776@gmail.com>
2024-09-30 19:42:28 +0530

    Time: 23 ms (19.62%) | Memory: 18.7 MB (32.79%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>indegree(numCourses,0);
-        vector<int> adj[numCourses];
-        
-		for (auto it : prerequisites) 
-			adj[it[1]].push_back(it[0]);
-		
-        for(int i=0;i<prerequisites.size();++i)
-            indegree[prerequisites[i][0]]++;
-        
-        queue<int>q;
-        for(int i=0;i<numCourses;++i)
-        {
-            if(indegree[i]==0)
-                q.push(i);
-        }
-        vector<int> topo;
-        while(!q.empty())
-        {
-           int t=q.front();
-            q.pop();
-            topo.push_back(t);
-            for(int v:adj[t])
-            {
-                indegree[v]--;
-                if(indegree[v]==0)
-                    q.push(v);
+    queue<int>q;
+    vector<vector<int>>adj(numCourses);
+    vector<int>input(numCourses,0);
+    for(int i=0;i<prerequisites.size();++i){
+        input[prerequisites[i][0]]++;
+        adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+    }
+    for(int i=0;i<numCourses;++i)
+        if(input[i]==0) q.push(i);
+ vector<int>top;
+    while(!q.empty()){
+           int currnode=q.front();
+           q.pop();
+            top.push_back(currnode);
+            for(int j=0;j<adj[currnode].size();++j){
+                    input[adj[currnode][j]]--;
+                    if(input[adj[currnode][j]]==0)
+                        q.push(adj[currnode][j]);
             }
         }
-        
-        if (topo.size() == numCourses) return true;
-		return false;
+        return top.size()==numCourses;
     }
 };
-// 0->1
