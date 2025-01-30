e35c035c4c9b2d0f60c138d2881e92abce617464
Ruved Aalam <ruvedalam7776@gmail.com>
2025-01-30 21:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (47.08%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-int help(vector<vector<int>>& books, int &shelfWidth,int i,vector<int>&dp){
+int help(vector<vector<int>>& books,int i, int &shelfWidth,vector<int>&dp){
+    
     if(i<0) return 0;
     if(dp[i]!=-1) return dp[i];
 
-    int ans=0;
-    int k=0;
+    int rmw=shelfWidth;
+    int height=0;
+    int ans=INT_MAX;
     int j=i;
-    int res=INT_MAX;
-    while(j>=0 &&shelfWidth>=k+books[j][0]){
-            k+=books[j][0];
-            ans=max(ans,books[j][1]);
-            res=min(res,ans+help(books,shelfWidth,j-1,dp));
-            --j;
+    while(i>=0 && (rmw >= books[i][0])){
+        height=max(height,books[i][1]);
+        int temp=help(books,i-1,shelfWidth,dp);
+        ans=min(ans,height+temp);
+        rmw-=books[i][0];
+        i--;
     }
-    return dp[i]=res;
+    return dp[j]=ans;
 }
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
         int n=books.size();
         vector<int>dp(n,-1);
-       return help(books,shelfWidth,books.size()-1,dp);
+        return help(books,n-1,shelfWidth,dp);
     }
 };
+
+
