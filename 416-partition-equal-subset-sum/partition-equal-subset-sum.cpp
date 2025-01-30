2bad11cc168b7b2eb5f74caf3b20ddc9e5737a23
Ruved Aalam <ruvedalam7776@gmail.com>
2025-01-30 20:46:00 +0530

    Time: 138 ms (37.67%) | Memory: 100.2 MB (45.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-int help(vector<int>& nums,int i,int sum,vector<vector<int>>&dp){
+bool help(vector<int>& nums,int i,int sum,vector<vector<int>>&dp){
     if(sum==0) return true;
     if(i<0) return false;
+
     if(dp[i][sum]!=-1) return dp[i][sum];
-    int take=false,notake=false;
-    if(sum>=nums[i]) 
+    
+    bool take=false;
+    if(nums[i]<=sum)    
         take=help(nums,i-1,sum-nums[i],dp);
-    notake=help(nums,i-1,sum,dp);
-    dp[i][sum]=take||notake;
-    return dp[i][sum];
+    bool notake=help(nums,i-1,sum,dp);
+
+    return dp[i][sum]=notake||take;
 }
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(int i=0;i<nums.size();++i) sum+=nums[i];
         int n=nums.size();
+        
+        for(int i=0;i<nums.size();++i) sum+=nums[i];
         if(sum%2==1) return false;
         vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
-       if (help(nums,n-1,sum/2,dp)==1)
-        return true;
-       return false;
+        return help(nums,nums.size()-1,sum/2,dp);
     }
 };
