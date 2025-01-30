03aa3ba352efda74a50839b7e38091f141589145
Ruved Aalam <ruvedalam7776@gmail.com>
2025-01-30 20:34:28 +0530

    Time: 74 ms (43.31%) | Memory: 14 MB (25.35%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int help(int n,vector<int>&tc,vector<int>&res){
-        if(n==0) return 0;
-        if(res[n]!=-1) return res[n];
-        int rt=sqrt(n);
-        int ans=INT_MAX;
-        for(int i=1;i<=rt;++i)
-            ans=min(ans,1+help(n-tc[i],tc,res));
-        return res[n]=ans;
-    }
+
+int help(int n,vector<int>&dp){
+    if(n<=1) return n;
+    if(dp[n]!=-1) return dp[n];
+    int sqroot=sqrt(n);
+    int sq=sqroot*sqroot;
+    int res=INT_MAX;
+    for(int i=1;i<=sqroot;++i)
+        res=min(res,1+help(n-i*i,dp));
+    return dp[n]=res;
+}   
     int numSquares(int n) {
-        int si=sqrt(n);
-        vector<int>dp(si+1,-1);
-        for(int i=1;i*i<=n;++i)
-            dp[i]=i*i;
-            vector<int>res(n+1,-1);
-        return help(n,dp,res);
+        vector<int>dp(n+1,-1);
+        return help(n,dp);
     }
 };
-
