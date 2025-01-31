02c53bf06c235e7e3c9f4c770b56f215f8cca701
Ruved Aalam <ruvedalam7776@gmail.com>
2025-01-31 19:20:19 +0530

    Time: 4 ms (23.31%) | Memory: 15.3 MB (74.62%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-int help(vector<int>& prices,int buy,int i,vector<vector<int>>&dp){
+
+int help(vector<int>& prices,bool buy,int i,vector<vector<int>>&dp){
     if(i>=prices.size()) return 0;
 
-    if(dp[buy+1][i]!=-1) return dp[buy+1][i];
+    if(dp[buy][i]!=-1) return dp[buy][i];
     int take=0,notake=0;
-    if(buy==-1)
-        take=help(prices,i,i+1,dp);
+    if(buy==true)
+        take=-prices[i]+help(prices,false,i+1,dp);
     else
-        take=(prices[i]-prices[buy])+help(prices,-1,i+2,dp);
+        take=prices[i]+help(prices,true,i+2,dp);
     notake=help(prices,buy,i+1,dp);
-    return dp[buy+1][i]=max(take,notake);
+    return dp[buy][i]=max(take,notake);
 }
     int maxProfit(vector<int>& prices) {
         int buy=-1;
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(n,-1));
+        vector<vector<int>>dp(2,vector<int>(n,-1));
         return help(prices,buy,0,dp);
     }
 };
