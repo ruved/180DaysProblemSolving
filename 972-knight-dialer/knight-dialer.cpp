8f3e8b74221ab753a0f262e314195792112d7e03
Ruved Aalam <ruvedalam7776@gmail.com>
2025-02-01 19:02:34 +0530

    Time: 117 ms (28.59%) | Memory: 41.3 MB (26.18%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-int mod=1e9+7;
-vector<vector<int>>numpadKnight={{4,6},{6,8},{7,9},{4,8},{0,3,9},{},{0,1,7},{2,6},{1,3},{2,4}};
-
-int help(int n,int pos,vector<vector<int>>&dp){
-    if(n<=0) return 1;
-    if(dp[pos][n]!=-1) return dp[pos][n];
-    long res=0;
-    for(int j=0;j<numpadKnight[pos].size();++j){
-        res+=help(n-1,numpadKnight[pos][j],dp);
-        res=res%mod;
-        }
-     return dp[pos][n]=res%=mod;
+vector<vector<int>>cord={{4,6},{6,8},{7,9},{4,8},{0,3,9},{},{0,1,7},{2,6},
+{1,3},{4,2}};
+long long mod=1e9+7;
+int help(int n,int i,vector<vector<int>>&dp){
+    if(n==1) return 1;
+    if(i==5) return 0;
+    if(dp[n][i]!=-1) return dp[n][i];
+    long long int ans=0;
+    for(int num:cord[i]){
+        int temp=help(n-1,num,dp);
+        ans=(ans%mod+temp%mod)%mod;
+    }
+    return dp[n][i]=ans;
 }
     int knightDialer(int n) {
-        long res=0;
-        vector<vector<int>>dp(10,vector<int>(n+1,-1));
+        long int ans=0;
+        vector<vector<int>>dp(n+1,vector<int>(10,-1));
         for(int i=0;i<=9;++i){
-            res+=help(n-1,i,dp);
-            res%=mod;
+            int temp=help(n,i,dp);
+            ans=(ans%mod+temp%mod)%mod;
         }
-        return res;
+        return ans%mod;
     }
 };
+// n=3
+// i=1 -> 6,8 // i=2 -> 7,9 // i=3 -> 4,8 // i=4 -> 3,9 // i=5 -> 
+// i=6 -> 1,7 // i=7 -> 2,6 // i=8 -> 1,3 // i=9 -> 4,1 // i=0 -> 4,6
