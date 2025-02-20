95b5f85cf032f12483d6827f793ba1519685f3d3
Ruved Aalam <ruvedalam7776@gmail.com>
2025-02-20 06:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (45.82%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,26 +1,27 @@
 class Solution {
-    int dfs(int i, int n, vector<int> &nums, unordered_map<int, int> &m, vector<int> &dp) {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-
-        int notpick = 0 + dfs(i + 1, n, nums, m, dp);
-        int pick = m[nums[i]] * nums[i];
-        if(i + 1 < n && nums[i] + 1 == nums[i + 1]) pick += dfs(i + 2, n, nums, m, dp);
-        else if(i + 1 < n && nums[i] + 1 != nums[i + 1]) pick += dfs(i + 1, n, nums, m, dp);
-        
-        return dp[i] = max(pick, notpick);
-    }
 public:
+int help(vector<int>& nums,int i,unordered_map<int,int>&mp,vector<int>&dp){
+    if(i<0) return 0;
+    if(dp[i]!=-1) return dp[i];
+    int take=0,notake=0;
+    if((i-1)>=0&&nums[i]-1==nums[i-1])
+        take=mp[nums[i]]*nums[i]+help(nums,i-2,mp,dp);
+    else
+        take=mp[nums[i]]*nums[i]+help(nums,i-1,mp,dp);
+    
+    notake=help(nums,i-1,mp,dp);
+    return dp[i]=max(take,notake);
+}
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for(int i : nums) m[i]++;
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;++i) mp[nums[i]]++;
 
-        vector<int> v;
-        for(auto i : m) v.push_back(i.first);
-        sort(v.begin(), v.end());
-        int n = v.size();
-        vector<int> dp(n, -1);
-
-        return dfs(0, n, v, m, dp);
+        vector<int>ar;
+        for(auto it:mp)
+            ar.push_back(it.first); 
+        sort(ar.begin(),ar.end());
+        vector<int>dp(ar.size(),-1);
+       return help(ar,ar.size()-1,mp,dp);
     }
 };
