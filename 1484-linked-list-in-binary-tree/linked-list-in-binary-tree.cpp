4e654425b9f95ff99b83df8fb52fb8bab71a554e
Ruved Aalam <ruvedalam7776@gmail.com>
2025-03-13 01:42:19 +0530

    Time: 3 ms (21.61%) | Memory: 32.1 MB (93.62%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -1,39 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSubPath(ListNode* head, TreeNode* root) {
-        // Build the pattern and prefix table from the linked list
-        vector<int> pattern = {head->val}, prefixTable = {0};
-        int patternIndex = 0;
-        head = head->next;
-
-        while (head) {
-            while (patternIndex && head->val != pattern[patternIndex])
-                patternIndex = prefixTable[patternIndex - 1];
-            patternIndex += head->val == pattern[patternIndex];
-            pattern.push_back(head->val);
-            prefixTable.push_back(patternIndex);
-            head = head->next;
-        }
-
-        // Perform DFS to search for the pattern in the tree
-        return searchInTree(root, 0, pattern, prefixTable);
+bool help(ListNode* head, TreeNode* root,bool parent){
+    if(!head) return true;
+    if(!root) return false;
+    bool ans=false;
+    if(head->val==root->val){
+        
+        ans|=help(head->next,root->left,true)||help(head->next,root->right,true);
     }
-
-private:
-    bool searchInTree(TreeNode* node, int patternIndex,
-                      const vector<int>& pattern,
-                      const vector<int>& prefixTable) {
-        if (!node) return false;
-
-        while (patternIndex && node->val != pattern[patternIndex])
-            patternIndex = prefixTable[patternIndex - 1];
-        patternIndex += node->val == pattern[patternIndex];
-
-        // Check if the pattern is fully matched
-        if (patternIndex == pattern.size()) return true;
-
-        // Recursively search left and right subtrees
-        return searchInTree(node->left, patternIndex, pattern, prefixTable) ||
-               searchInTree(node->right, patternIndex, pattern, prefixTable);
+    if(!parent)
+    ans|=help(head,root->left,false)||help(head,root->right,false);
+    return ans;
+}
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        return help(head,root,false);
     }
 };
