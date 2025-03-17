ae219bed66fa86e6973d164dc8ac45384849917b
Ruved Aalam <ruvedalam7776@gmail.com>
2025-03-18 05:12:53 +0530

    Time: 31 ms (52.73%) | Memory: 17.5 MB (35.71%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    pair<int, int> solveMem(vector<int>& nums, int curr, int prev, vector<vector<pair<int, int>>>& dp) {
-        if (curr == nums.size()) return make_pair(0, 1);
-
-        if (dp[curr][prev + 1].first != -1) return dp[curr][prev + 1];
-
-        int length = 0, count = 0;
-
-        if (prev == -1 || nums[prev] < nums[curr]){
-            pair<int, int> include = solveMem(nums, curr + 1, curr, dp);
-            int includeLength = 1 + include.first;
-            if (includeLength > length){
-                length = includeLength;
-                count = include.second;
-            } 
-            else if (includeLength == length){
-                count += include.second;
-            }
-        }
-
-        pair<int, int> exclude = solveMem(nums, curr + 1, prev, dp);
-        if (exclude.first > length){
-            length = exclude.first;
-            count = exclude.second;
-        } 
-        else if (exclude.first == length){
-            count += exclude.second;
-        }
-
-        dp[curr][prev + 1] = make_pair(length, count);
-
-        return dp[curr][prev + 1];
-    }
+    
     int findNumberOfLIS(vector<int>& nums) {
-        int curr = 0, prev = -1;
-        vector<vector<pair<int, int>>> dp(nums.size(), vector<pair<int, int>>(nums.size() + 1, make_pair(-1, -1)));
-        pair<int, int> result = solveMem(nums, curr, prev, dp);
-        return result.second;
+        const int n = nums.size();
+        vector<int> lis(n,1);  
+        vector<int> count(n,1);  
+        int maxLen = 1;  
+		
+		// O(N^2) DP Solution
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(lis[j] + 1 > lis[i]){ // strictly increasing
+                        lis[i] = lis[j] + 1;
+                        count[i] = count[j];
+                    } 
+					
+					else if(lis[j]+1 == lis[i]){ 
+                        count[i] += count[j];
+                    }
+                }
+            }
+            maxLen = max(maxLen,lis[i]);
+        }
+        
+        int numOfLIS = 0;
+        // count all the subseq of length maxLen
+        for(int i=0;i<n;i++){
+            if(lis[i]==maxLen)
+                numOfLIS += count[i];
+        }
+            
+        return numOfLIS;
     }
 };
