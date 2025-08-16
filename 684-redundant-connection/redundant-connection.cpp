ebf607bd497474535f70b15d6b565f4479df9536
Ruved Aalam <ruvedalam7776@gmail.com>
2025-08-16 18:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (54.72%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,23 +1,49 @@
-class Solution {
+class DisjointSet {
 public:
-    vector<int> parent;
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        if(edges.size()==0) return{};
-        parent.resize(edges.size()+1);
-        for(int i=1;i<edges.size()+1;i++){
+    vector<int> parent,size;
+    DisjointSet(int n) {
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for (int i=1;i<=n;i++){
             parent[i]=i;
         }
-        for(vector<int> edge : edges){
-            int f1=find(edge[0]);
-            int f2=find(edge[1]);
-            if(f1!=f2)
-                parent[f1]=f2;
-            else
-                return edge;
-        }
-        return {};
     }
-    int find(int x){
-        return parent[x]==x ? x : find(parent[x]);
+
+    int findUPar(int node) {
+        if (parent[node]==node) return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+
+    void unionBySize(int u,int v) {
+        int pu=findUPar(u);
+        int pv=findUPar(v);
+        if (pu==pv) return;
+        if (size[pu]<size[pv]) {
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }
+        else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        DisjointSet ds(n);
+        vector<int> ans(2);
+        for (auto i:edges){
+            int u=ds.findUPar(i[0]);
+            int v=ds.findUPar(i[1]);
+            if (u==v) {
+                ans[0]=i[0];
+                ans[1]=i[1];
+            }
+            else ds.unionBySize(u,v);
+        }
+        return ans;
     }
 };
