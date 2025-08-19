a098735beb88fb302aa2d13f7e944551f247a7c4
Ruved Aalam <ruvedalam7776@gmail.com>
2025-08-19 23:01:12 +0530

    Time: 4 ms (82.43%) | Memory: 27.1 MB (5.15%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
+    int minFallingPathSum(vector<vector<int>>& points) {
+         int m = points.size(), n = points[0].size();
 
-    int minFallingPathSum(vector<vector<int>>& grid) {
-      int n = grid.size(), res = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        // dp[c] = best score ending at column c in previous row
+        vector<long long> dp(points[0].begin(), points[0].end());
 
-        for(int j = 0; j < n; ++j) {
-            dp[0][j] = grid[0][j];
-        }
+        for (int r = 1; r < m; r++) {
+            vector<long long> new_dp(n, 0);
 
-        for(int i = 1; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                int temp = INT_MAX;
-
-                for(int k = 0; k < n; ++k) {
-                    if(k != j) {
-                        temp = min(temp, grid[i][j] + dp[i - 1][k]);
-                    }
-
-                    dp[i][j] = temp;
-                }
+            // Step 1: build prefixMax for left case
+            vector<long long> prefix(n);
+            prefix[0] = dp[0] + 0;
+            for (int c = 1; c < n; c++) {
+                prefix[c] = min(prefix[c - 1], dp[c]);
             }
+
+            // Step 2: build suffixMax for right case
+            vector<long long> suffix(n);
+            suffix[n - 1] = dp[n - 1] ;
+            for (int c = n - 2; c >= 0; c--) {
+                suffix[c] = min(suffix[c + 1], dp[c]);
+            }
+
+            // Step 3: compute new dp for row r
+            for (int c = 0; c < n; c++) {
+                long long best = 0;
+                if(c==0) best=suffix[c+1];
+                else if(c==n-1) best=prefix[c-1];
+                else  best = min(prefix[c-1] , suffix[c+1]);
+                new_dp[c] = points[r][c] + best;
+            }
+
+            dp.swap(new_dp); // move to next row
         }
 
-        for(int j = 0; j < n; ++j) {
-            res = min(res, dp[n - 1][j]);
-        }
-
-        return res;
+        return *min_element(dp.begin(), dp.end());
     }
 };
