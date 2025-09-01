82f43b12a8fd6987265279962aa706a754aaa9ee
Ruved Aalam <ruvedalam7776@gmail.com>
2025-09-02 02:28:10 +0530

    Time: 4 ms (71.85%) | Memory: 12.4 MB (87.49%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-int help(vector<int>& arr, int &k,int i,vector<int>&dp){
-    if(i<0) return 0;
-    if(dp[i]!=-1) return dp[i];
-    int mx=-1;
-    int ans=0;
-    for(int j=i;j>(i-k)&&(j>=0);--j){
-        mx=max(mx,arr[j]);
-        ans=max(ans,mx*(i-j+1)+help(arr,k,j-1,dp));
-    }
-    return dp[i]=ans;
-}
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int>dp(n+1,-1);
-        return help(arr,k,n-1,dp);
+        vector<int>dp(n,0);
+        for(int i=0;i<n;++i){
+            int mx=INT_MIN;
+            for(int j=0;j<k&&(i-j)>=0;++j){
+                mx=max(arr[i-j],mx);
+                int prev = (i - j - 1 >= 0) ? dp[i - j - 1] : 0;   
+                dp[i] = max(dp[i], prev + (j + 1) * mx);
+            }
+           // cout<<dp[i]<<"-";
+        }
+        return dp[n-1];
     }
 };
-
