e4c1ff289efcff2328478cb986d594530eea680f
Ruved Aalam <ruvedalam7776@gmail.com>
2025-09-09 02:38:41 +0530

    Time: 3 ms (61.41%) | Memory: 18.1 MB (57.04%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for(auto flight : flights){
-            
-            adj[flight[0]].push_back({flight[1], flight[2]});
-        }
-        //it will store [node, cost]
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        //it will store minimum cost to reach each node
-        vector<int> minCost(n, INT_MAX);
-        int stops = 0;
-        while(!q.empty() && stops <= k){
-            int size = q.size();
-            while (size--) {
-                auto [currNode, cost] = q.front();
-                q.pop();
-                for (auto& [neighbour, price] : adj[currNode]) {
-                    if (price + cost < minCost[neighbour]){
-                        minCost[neighbour] = price + cost;
-                        q.push({neighbour, minCost[neighbour]});
-                    }
-                }
-            }
-            stops++;
-        }
-        if(minCost[dst] == INT_MAX)
-            return -1;
-        return minCost[dst];
+struct State {
+    int cost;   // total price so far
+    int city;   // current city
+    int stops;  // number of stops used so far
+    bool operator>(const State& other) const {
+        return cost > other.cost;
     }
+};
+
+int findCheapestPrice(int n, vector<vector<int>>& flights,
+                              int src, int dst, int k) {
+    // Build adjacency list: city -> {neighbor, price}
+    vector<vector<pair<int,int>>> graph(n);
+    for (auto &f : flights) {
+        int u = f[0], v = f[1], w = f[2];
+        graph[u].push_back({v, w});
+    }
+
+    priority_queue<State, vector<State>, greater<State>> pq;
+    pq.push({0, src, 0}); // cost, city, stops
+
+    vector<int> minStops(n, INT_MAX); // track minimal stops used to reach city
+
+    while (!pq.empty()) {
+        auto [cost, u, stops] = pq.top(); pq.pop();
+
+        if (u == dst) return cost;
+
+        if (stops > k || stops >= minStops[u]) continue;
+        minStops[u] = stops;
+
+        for (auto &[v, w] : graph[u]) {
+            pq.push({cost + w, v, stops + 1});
+        }
+    }
+
+    return -1;
+}
+   
 };
