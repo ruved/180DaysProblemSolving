db55d647b39fb118d7a66eaa3f0a317be18798c3
Ruved Aalam <ruvedalam7776@gmail.com>
2025-10-21 23:49:56 +0530

    Time: 13 ms (92.23%) | Memory: 91.6 MB (87.38%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,38 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        sort(nums.begin(), nums.end());
-
-        unordered_map<int,int> freq;
-        freq.reserve(n*2);
-        for (int x : nums) ++freq[x];
-
-        vector<int> uniq;
-        uniq.reserve(freq.size());
-        for (auto &p : freq) uniq.push_back(p.first);
-        sort(uniq.begin(), uniq.end());
-
-        int ans = 0;
-        for (int v : uniq) {
-            int L = int(lower_bound(nums.begin(), nums.end(), v - k) - nums.begin());
-            int R = int(upper_bound(nums.begin(), nums.end(), v + k) - nums.begin());
-            int candidate = R - L;                     // elements that can reach v
-            int achievable = min(candidate, freq[v] + numOperations);
-            ans = max(ans, achievable);
+    static int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        static constexpr int N=1e5+2;
+        int freq[N]={0}, sweep[N]={0}, mm=N, MM=0;
+        for(int x: nums){
+            freq[x]++;
+            const int x0=max(1, x-k), xN=min(x+k+1, N-1);
+            sweep[x0]++;
+            sweep[xN]--;
+            mm=min(mm, x0);
+            MM=max(MM, xN);
         }
-
-        int l = 0, maxcand = 0;
-        for (int r = 0; r < n; ++r) {
-            while (nums[r] - nums[l] > 2LL * k) ++l;
-            maxcand = max(maxcand, r - l + 1);
+        int ans=0, cnt=0;
+        for (int x=mm; x<=MM; x++){
+            cnt+=sweep[x];
+            ans=max(ans, freq[x]+min(cnt-freq[x], numOperations));
         }
-        ans = max(ans, min(maxcand, numOperations));
-
         return ans;
     }
 };
