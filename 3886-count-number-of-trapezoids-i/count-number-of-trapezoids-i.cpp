53942d6deafd89974c40a744d6fc99a8333eaf47
Ruved Aalam <ruvedalam7776@gmail.com>
2025-12-03 00:59:12 +0530

    Time: 287 ms (9.73%) | Memory: 284.7 MB (5.26%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -8,17 +8,18 @@ int mod=1e9+7;
             mp[points[i][1]].insert(points[i][0]);
         unordered_map<int,int>umap;
         long total=0;
+        vector<int>left;
         for(auto it=mp.begin();it!=mp.end();++it){
             long temp=it->second.size();
             temp=(temp*(temp-1))/2;
-            umap[it->first]=temp%mod;
+            left.push_back(temp%mod);
             total+=temp;
             total%=mod;
         }
         
         long ans=0;
-        for(auto it=umap.begin();it!=umap.end();++it){
-            long  up=it->second;
+        for(int i=0;i<left.size();++i){
+            long  up=left[i];
             long bottom=total-up;
             total-=up;
             ans+=(up%mod*bottom%mod)%mod;
