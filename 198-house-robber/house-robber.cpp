b5e5637118ba62147574d09465b787091ac80f22
Ruved Aalam <ruvedalam7776@gmail.com>
2026-01-18 16:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,9 +3,7 @@ public:
 int help(vector<int>& nums,int i,vector<int>&dp){
     if(i<0) return 0;
     if(dp[i]!=-1) return dp[i];
-    int take=nums[i]+help(nums,i-2,dp);
-    int notake=help(nums,i-1,dp);
-    return dp[i]=max(take,notake);
+    return dp[i]=max(nums[i]+help(nums,i-2,dp),help(nums,i-1,dp));
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
