0ad6bd049a26a462ded62a454c3560ebf2df2d86
Ruved Aalam <ruvedalam7776@gmail.com>
2026-01-18 22:25:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (83.44%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid[0].size();
-        int m=obstacleGrid.size();
-        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1) return 0;
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<int> dp(n, 0);
 
-        vector<int>dp(n,0);
-        for(int i=0;i<n;++i){
-            if(obstacleGrid[0][i]!=0) break;
-            dp[i]=1;
-        }
-        bool f=true;
-        for(int i=1;i<m;++i){
-            if(obstacleGrid[i][0]==1&&f) {
-                f=false;
-            }
-            dp[0]=f;
-            for(int j=1;j<n;++j){
-                if(obstacleGrid[i][j]==0)
-                    dp[j]=dp[j]+dp[j-1];
-                else
-                    dp[j]=0;
+        dp[0] = (obstacleGrid[0][0] == 0);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;                  // blocked
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];         // top + left
+                }
             }
         }
 
-        return dp[n-1];
+        return dp[n - 1];
     }
 };
