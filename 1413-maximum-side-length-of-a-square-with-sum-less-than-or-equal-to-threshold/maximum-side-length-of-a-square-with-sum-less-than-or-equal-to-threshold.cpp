8e27ed9cca7b67f1590acf760725fcf0a8db4252
Ruved Aalam <ruvedalam7776@gmail.com>
2026-01-20 01:15:33 +0530

    Time: 10 ms (71.87%) | Memory: 32.7 MB (78.57%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> pref(m + 1, vector<int>(n + 1, 0));
+    int m = mat.size(), n = mat[0].size();
+    vector<vector<int>> pref(m + 1, vector<int>(n + 1, 0));
+    int ans = 0;
 
-        // 1. Build 2D Prefix Sum
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                pref[i][j] = mat[i-1][j-1] + pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
-            }
-        }
-
-        // 2. Binary Search for the maximum side length
-        int low = 1, high = min(m, n), ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (hasSquare(pref, mid, threshold)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return ans;
-    }
-
-private:
-    bool hasSquare(vector<vector<int>>& pref, int L, int threshold) {
-        for (int i = L; i < pref.size(); ++i) {
-            for (int j = L; j < pref[0].size(); ++j) {
-                // Use the 2D Prefix Sum formula
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            pref[i][j] = mat[i-1][j-1] + pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
+            
+            // Check if a square of side (ans + 1) exists ending at (i, j)
+            int L = ans + 1;
+            if (i >= L && j >= L) {
                 int currentSum = pref[i][j] - pref[i-L][j] - pref[i][j-L] + pref[i-L][j-L];
-                if (currentSum <= threshold) return true;
+                if (currentSum <= threshold) {
+                    ans = L; // If valid, we try to find a larger one in the next iterations
+                }
             }
         }
-        return false;
     }
+    return ans;
+}
 };
