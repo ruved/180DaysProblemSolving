88025b29f3aeb43c3c957ac359a52dfe87bcda60
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-02 00:10:34 +0530

    Time: 2 ms (10.22%) | Memory: 8.6 MB (28.75%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-int help(int sum,vector<int>&dp){
-    if(sum<=1) return 1;
-    if(dp[sum]!=-1) return dp[sum];
-    int ans=0;
-    for(int i=1;i<=sum;++i){
-        int temp=i*help(sum-i,dp);
-        ans=max(ans,temp);
-    }
-    return dp[sum]=ans;
-}
     int integerBreak(int n) {
-        if(n==2) return 1;
-        if(n==3) return 2;
-        vector<int>dp(n+1,-1);
-        return help(n,dp);
+        if(n==1) return n;
+        int res=1;
+    vector<int>dp(n+1,0);
+    dp[1]=1;
+    for(int i=2;i<=n;++i){
+        for(int j=1;j<i;++j)
+            dp[i]=max(dp[i],j*max(i-j,dp[i-j]));
+        cout<<dp[i]<<" - ";
+    }
+        
+    return dp[n];
+
     }
 };
-
-// sum=x1+...+xn
-// product=x1*...*xn
-
-// 1+1+2+2+2+2= 2*2*2*2
-// 1+2+3+4= mul 24
-
