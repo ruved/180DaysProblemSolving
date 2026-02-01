1aa2312608db6534f4cfc0689f2a7bee2b4b6f39
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-01 18:31:02 +0530

    Time: 1091 ms (5.01%) | Memory: 13.5 MB (43.49%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-
-int help(int n,vector<int>&dp){
-    if(n<=1) return n;
-    if(dp[n]!=-1) return dp[n];
-    int sqroot=sqrt(n);
-    int sq=sqroot*sqroot;
-    int res=INT_MAX;
-    for(int i=1;i<=sqroot;++i)
-        res=min(res,1+help(n-i*i,dp));
-    return dp[n]=res;
-}   
     int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-        return help(n,dp);
+        if(n<=1) return n;
+        int l=sqrt(n);
+        vector<int>prev(l+1),dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=l;++i) prev[i]=i*i;
+        // int l=sqrt(n);
+        // int res=INT_MAX;
+        // for(int i=1;i<=l;++i){
+        //     res=min(res,1+numSquares(n-i*i));
+        // }
+        // return res;
+        int res=INT_MAX;
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=sqrt(i);++j)
+                    dp[i]=min(dp[i],1+dp[i-prev[j]]);
+            cout<<dp[i]<<" ";
+        }
+        return dp[n];
+
     }
 };
