5378d2936c00ff2fe0761dc8c6cd518abe72d6a5
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-08 00:59:04 +0530

    Time: 1 ms (99.36%) | Memory: 10.4 MB (85.37%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-int help(int amount, vector<int>& coins,int i,vector<vector<int>>&dp){
-    if(amount==0) return 1;
-    if(i<0) return 0;
-
-    if(dp[amount][i]!=-1) return dp[amount][i];
-    int take=0;
-    if(coins[i]<=amount)
-        take=help(amount-coins[i],coins,i,dp);
-    int notake=help(amount,coins,i-1,dp);
-    return dp[amount][i]=take+notake;
-}
-
-
     int change(int amount, vector<int>& coins) {
-       int n=coins.size();
-        vector<unsigned int>dp(amount+1,0);
+        int n=coins.size();
+        // vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
+        // for(int i=0;i<=n;++i) dp[i][0]=1;
+        vector<long  unsigned>dp(amount+1,0);
         dp[0]=1;
-        for(int i=0;i<n;i++){
-            for(int j=coins[i];j<=amount;j++){
-                dp[j]+=dp[j-coins[i]];
+        for(int i=0;i<n;++i){
+            for(int am=coins[i];am<=amount;++am){
+               dp[am]+=dp[am-coins[i]];
             }
         }
-        return dp[amount];
+        return (int)dp[amount];
     }
 };
