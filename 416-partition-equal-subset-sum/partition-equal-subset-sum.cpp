b1caf75b8d2813485e86d2ed661cc66c47c742eb
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-13 01:48:50 +0530

    Time: 84 ms (76.32%) | Memory: 13.4 MB (86.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -16,22 +16,30 @@ int help(vector<int>& nums,int i,int sum, vector<vector<int>>&dp){
         if(sum&1) return false;
         // vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
         // return help(nums,nums.size()-1,sum/2,dp);
-        vector<vector<bool>>dp(n,vector<bool>(sum/2+1,false));
-        for(int i=0;i<n;++i) dp[i][0]=true;
-        if(nums[0]<=sum/2)
-            dp[0][nums[0]]=true;
-        // vector<bool>dp(sum/2+1,false);
-        // dp[0]=true;
-        // dp[0][nums[0]]=true;
-        for(int i=1;i<n;++i){
-            for(int am=1;am<=sum/2;++am){
-                if(nums[i]>am)
-                dp[i][am]=dp[i-1][am];
-                else
-                dp[i][am]=dp[i-1][am]|dp[i-1][am-nums[i]];
-            }
-        }
-        return dp[n-1][sum/2];
+        // vector<vector<bool>>dp(n,vector<bool>(sum/2+1,false));
+        // for(int i=0;i<n;++i) dp[i][0]=true;
+        // if(nums[0]<=sum/2)
+        //     dp[0][nums[0]]=true;
+       
+        // for(int i=1;i<n;++i){
+        //     for(int am=1;am<=sum/2;++am){
+        //         if(nums[i]>am)
+        //         dp[i][am]=dp[i-1][am];
+        //         else
+        //         dp[i][am]=dp[i-1][am]|dp[i-1][am-nums[i]];
+        //     }
+        // }
+        // return dp[n-1][sum/2];
+        vector<bool>dp(sum/2+1,false);
+        dp[0]=true;
+        // if(nums[0]<=(sum/2))
+        //     dp[nums[0]]=true;
         
+        for(int i=0;i<n;++i)
+           for(int am = sum/2; am >= nums[i]; am--) {
+                dp[am] = dp[am] || dp[am - nums[i]];
+    }
+            
+        return dp[sum/2];
     }
 };
