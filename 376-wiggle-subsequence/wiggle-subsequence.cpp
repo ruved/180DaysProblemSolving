e057da8a2b17134625c44f8dd46b3fe5506cf450
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-12 22:47:16 +0530

    Time: 9 ms (13.88%) | Memory: 11 MB (18.62%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-int help(vector<int>& nums,int i,bool pos,vector<vector<int>>&dp){
-    if(i<0) return 0;
-    if(dp[i][pos]!=-1) return dp[i][pos];
-    int notake=help(nums,i-1,pos,dp);
-    int take=1;
-    for(int j=i-1;j>=0;--j){
-        if((pos&&nums[i]-nums[j]>0)||(!pos&&nums[i]-nums[j]<0))
-            take=max(take,1+help(nums,j,!pos,dp));
+int help(vector<int>& nums,int prev,bool pos,vector<vector<int>>&dp){
+    if(prev<=0) return prev;
+    if(dp[prev][pos]!=-1) return dp[prev][pos];
+     int len=0;
+    for(int j=prev-1;j>=0;--j){
+        if(pos==((nums[prev]-nums[j])<0)&&nums[prev]!=nums[j])
+            len=max(len,1+help(nums,j,!pos,dp));
     }
-    return dp[i][pos]=max(take,notake);
+   
+    return dp[prev][pos]=len;
+
 }
     int wiggleMaxLength(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(2,-1));
-        return max(help(nums,n-1,true,dp),help(nums,n-1,false,dp));
+        int l=max(help(nums,n-1,true,dp),help(nums,n-1,false,dp));
+        return l+1;
     }
 };
