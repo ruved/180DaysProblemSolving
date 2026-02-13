1a6d283575a77eadc754de5f34936e10283300d3
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-14 04:02:40 +0530

    Time: 131 ms (5.34%) | Memory: 108.4 MB (8.15%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-int help(vector<int>& prices,bool buy,int i,vector<vector<int>>&dp,int &fee){
+int help(vector<int>& prices, int &fee,int i,bool canbuy,vector<vector<int>>&dp){
     if(i>=prices.size()) return 0;
-
-    if(dp[buy][i]!=-1) return dp[buy][i];
-    int take=0,notake=0;
-    if(buy==true)
-        take=-prices[i]+help(prices,false,i+1,dp,fee);
-    else
-        take=(prices[i]-fee)+help(prices,true,i+1,dp,fee);
-    notake=help(prices,buy,i+1,dp,fee);
-    return dp[buy][i]=max(take,notake);
+    if(dp[i][canbuy]!=-1) return dp[i][canbuy];
+    int add=canbuy?-prices[i]:prices[i]-fee;
+    int take=add+help(prices,fee,i+1,!canbuy,dp);
+    int skip=help(prices,fee,i+1,canbuy,dp);
+    return dp[i][canbuy]=max(take,skip);
 }
     int maxProfit(vector<int>& prices, int fee) {
-        int buy=-1;
         int n=prices.size();
-        vector<vector<int>>dp(2,vector<int>(n,-1));
-        return help(prices,buy,0,dp,fee);
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return help(prices,fee,0,true,dp);
     }
 };
