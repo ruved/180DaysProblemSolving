ccf6d12f1fc2026ad49242d1771eca1aa3908155
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-14 21:17:24 +0530

    Time: 58 ms (5.99%) | Memory: 46.4 MB (5.86%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
 int help(vector<int>& nums,int i,int rem,vector<vector<int>>&dp){
-    if(i<0) {return (rem==0)?0:INT_MAX;}
+    if(i<0) {return (rem==0)?0:INT_MIN;}
     if(dp[i][rem]!=-1) return dp[i][rem];
     int take=help(nums,i-1,(rem+nums[i])%3,dp);
-    if(take!=INT_MAX) take+=nums[i];
+    if(take!=INT_MIN) take+=nums[i];
     int notake=help(nums,i-1,rem,dp);
-    int res=0;
-    if(take==INT_MAX&&notake==INT_MAX) res=INT_MAX;
-    else if(take==INT_MAX||notake==INT_MAX) res=min(take,notake);
-    else
-        res=max(take,notake);
-    return dp[i][rem]=res;
+    return dp[i][rem]=max(take,notake);
 }
     int maxSumDivThree(vector<int>& nums) {
         int n=nums.size();
