6e423aac39c3abb82f931cbfd738ba87992b6290
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-14 16:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (51.89%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -18,7 +18,22 @@ int help(vector<int>& days, vector<int>& costs,int i,vector<int>&dp){
 }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
-        vector<int>dp(n,-1);
-        return help(days,costs,n-1,dp);
+        vector<int>dp(n,0);
+        // return help(days,costs,n-1,dp);
+        dp[0]=costs[0];
+        for(int i=0;i<n;++i){
+            int j=i;
+            j--;
+            int day=costs[0]+(j<0?0:dp[j]);
+            while(j>=0&&days[i]-7<days[j])
+                j--;
+            int week=costs[1]+(j<0?0:dp[j]);
+            j=i;
+            while(j>=0&&days[i]-30<days[j])
+                j--;
+            int month=costs[2]+(j<0?0:dp[j]);
+            dp[i]=min(min(day,week),month);
+        }
+        return dp[n-1];
     }
 };
