8efb2901d9a12e90cae5d1c411b7f19e01d46e8c
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-14 12:30:10 +0530

    Time: 3 ms (60.24%) | Memory: 18 MB (26.08%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-int help(vector<int>& nums,int i,unordered_map<int,int>&mp,vector<int>&dp){
+int help(vector<pair<int,int>>&arr,int i,vector<int>&dp){
     if(i<0) return 0;
+    if(i==0) return arr[0].first*arr[0].second;
     if(dp[i]!=-1) return dp[i];
-    int take=0,notake=0;
-    if((i-1)>=0&&nums[i]-1==nums[i-1])
-        take=mp[nums[i]]*nums[i]+help(nums,i-2,mp,dp);
-    else
-        take=mp[nums[i]]*nums[i]+help(nums,i-1,mp,dp);
-    
-    notake=help(nums,i-1,mp,dp);
-    return dp[i]=max(take,notake);
+
+    int cur=arr[i].first*arr[i].second;
+    int take=0;
+    if(i>0&&arr[i].first==arr[i-1].first+1)
+        take=help(arr,i-2,dp);
+    else 
+        take=help(arr,i-1,dp);
+    int skip=help(arr,i-1,dp);
+
+    return dp[i]=max(cur+take,skip);;;
 }
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        int n=nums.size();
-        for(int i=0;i<n;++i) mp[nums[i]]++;
-
-        vector<int>ar;
-        for(auto it:mp)
-            ar.push_back(it.first); 
-        sort(ar.begin(),ar.end());
-        vector<int>dp(ar.size(),-1);
-       return help(ar,ar.size()-1,mp,dp);
+        map<int,int>mp;
+        for(int i=0;i<nums.size();++i) mp[nums[i]]++;
+        vector<pair<int,int>>arr;
+        for(auto it=mp.begin();it!=mp.end();++it)
+            arr.push_back({it->first,it->second});
+        
+        vector<int>dp(arr.size(),-1);
+        return help(arr,arr.size()-1,dp);
     }
 };
