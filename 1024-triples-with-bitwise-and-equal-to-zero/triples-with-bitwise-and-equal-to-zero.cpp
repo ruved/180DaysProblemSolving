c91165ad6ef8941e7dfdcf349142af175d29b273
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-15 20:57:39 +0530

    Time: 190 ms (58.65%) | Memory: 16.4 MB (75.53%) - LeetSync

--- 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.cpp
+++ 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
 
     int countTriplets(vector<int>& nums) {
-        int n=nums.size();
-       unordered_map<int,int>mp;
-       for(int i=0;i<n;++i)
-        for(int j=0;j<n;++j)
-            mp[nums[i]&nums[j]]++;
         
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();++i)
+            for(int j=0;j<nums.size();++j)
+                mp[nums[i]&nums[j]]++;
+
         int ans=0;
         for(auto it:mp){
-            for(int k=0;k<n;++k){
-                if((it.first&nums[k])==0)
-                    ans+=it.second;
-            }
+                if(it.first==0) ans+=(it.second*nums.size());
+                else {
+                    for(int i=0;i<nums.size();++i)
+                        if((it.first&nums[i])==0) ans+=it.second;
+                    }   
         }
-        return ans;
-        
+
+        return ans;   
     }
 };
