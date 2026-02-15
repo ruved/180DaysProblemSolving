e60d1a16a79d00f2508bba47a20114da8ab318f7
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-16 01:20:44 +0530

    Time: 27 ms (61.04%) | Memory: 141.6 MB (44.86%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int help(vector<int>& stoneValue,int i,vector<int>&dp){
-                
-        if(i==stoneValue.size()) return 0;
-        if(dp[i]!=INT_MIN) return dp[i];
-        int sum=0;
-        int temp=INT_MIN;
-        for(int j=i;j<=i+2&&j<stoneValue.size();++j){
-            sum+=stoneValue[j];
-            int temp2=sum-help(stoneValue,j+1,dp);
-            temp=max(temp,temp2);
+int help(vector<int>& stoneValue,int i,vector<int>&dp){
+    if(i>=stoneValue.size()) return 0;
+    
+    if(dp[i] != INT_MIN) return dp[i];
+    
+    int n = stoneValue.size();
+        int takeSum = 0;
+        int best = INT_MIN;
+
+        for(int k = 0; k < 3 && i + k < n; k++) {
+            takeSum += stoneValue[i + k];
+            best = max(best, takeSum - help(stoneValue, i + k + 1, dp));
         }
-        return dp[i]=temp;
-    }
+
+        return dp[i] = best;
+}
     string stoneGameIII(vector<int>& stoneValue) {
         int n=stoneValue.size();
         vector<int>dp(n,INT_MIN);
         int ans=help(stoneValue,0,dp);
         if(ans>0) return "Alice";
-        else if(ans<0) return "Bob";
-        else return "Tie";
+        else if(ans==0) return "Tie";
+        else return "Bob";
     }
 };
