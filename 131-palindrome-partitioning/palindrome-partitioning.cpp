490538f669d3779a45d33b3a0725d47885cd0144
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-17 01:39:23 +0530

    Time: 119 ms (14.35%) | Memory: 140.7 MB (13.00%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -3,32 +3,33 @@ public:
 bool checkpal(string s){
     int i=0,j=s.length()-1;
     while(i<j){
-        if(s[i]!=s[j]) return false;
+        if(s[i]!=s[j])
+            return false;
         i++;
         j--;
     }
     return true;
 }
-void help(string s,int i,vector<vector<string>>&ans,vector<string>&temp){
-    if(i==s.length()) {
-        if(temp.size()>0)
-        ans.push_back(temp);
+void help(string &s,int i,vector<string>temp,vector<vector<string>>&res){
+    if(i>=s.length()){
+        res.push_back(temp);
         return ;
     }
+    string temp2="";
     for(int j=i;j<s.length();++j){
-        string sub=s.substr(i,j-i+1);
-        if(checkpal(sub)){
-            temp.push_back(sub);
-            help(s,j+1,ans,temp);
+        temp2+=s[j];
+        if(checkpal(temp2)){
+            temp.push_back(temp2);
+            help(s,j+1,temp,res);
             temp.pop_back();
         }
+            
     }
-
 }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
         vector<string>temp;
-        help(s,0,ans,temp);
-        return ans;
+        vector<vector<string>>res;
+        help(s,0,temp,res);
+        return res;
     }
 };
