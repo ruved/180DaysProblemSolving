f820766d58371c6c454b2c879ed3d29df2863a92
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-16 22:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (5.16%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int help(string &s, string &t,int i,int j,vector<vector<int>>&dp){
+    if(i<0) return true;
+    if(j<0) return false;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==t[j])
+        return dp[i][j]= help(s,t,i-1,j-1,dp);
+    else
+        return dp[i][j]=help(s,t,i,j-1,dp);
+    
+}
+    bool isSubsequence(string s, string t) {
+        int n=s.length();
+        int m=t.length();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return help(s,t,n-1,m-1,dp);
+    }
+};
