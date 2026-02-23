ce5dfc453bf765b04dad6c42961afad2306bc0c0
Ruved Aalam <ruvedalam7776@gmail.com>
2026-02-24 00:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (68.41%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-int help(vector<int>& nums, int target,int i){
-    if(i<0){
-        if(target==0) return 1;
-        else return 0;
-    }
-    return help(nums,target-nums[i],i-1)+help(nums,target+nums[i],i-1);
-}
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size()-1;
-        return help(nums,target,n);
+       int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        // Impossible cases
+        if (abs(target) > totalSum) return 0;
+        if ((target + totalSum) % 2 != 0) return 0;
+
+        int subsetSum = (target + totalSum) / 2;
+
+        vector<int> dp(subsetSum + 1, 0);
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int s = subsetSum; s >= num; s--) {
+                dp[s] += dp[s - num];
+            }
+        }
+
+        return dp[subsetSum];
     }
 };
