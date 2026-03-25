45ff48b2d2e55e0aeab99123bbad5f87be5ddf90
Ruved Aalam <ruvedalam7776@gmail.com>
2026-03-25 16:16:41 +0530

    Time: 43 ms (73.90%) | Memory: 21 MB (29.07%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
 int help(vector<vector<int>>& grid,int i,int j1,int j2,vector<vector<vector<int>>>&dp){
-    
     if(i>=grid.size()) return 0;
-    if(j1<0||j1>=grid[0].size()||j2<0||j2>=grid[0].size()) return INT_MIN;
-
+    if(j1<0||j1>=grid[0].size()||j2<0||j2>=grid[0].size())
+    return INT_MIN;
     if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-    int ans=grid[i][j1];;
-    if(j1!=j2)
-        ans+=grid[i][j2];
-
-    int res=0;
-    for(int newcol1=j1-1;newcol1<=j1+1;++newcol1)
-        for(int newcol2=j2-1;newcol2<=j2+1;++newcol2)
-            res=max(res,help(grid,i+1,newcol1,newcol2,dp));
-
-    return dp[i][j1][j2]=(res!=INT_MIN)?res+ans:res;
+    int ans=INT_MIN;
+    
+    for(int k=-1;k<=1;++k)
+        for(int l=-1;l<=1;++l)
+            ans=max(ans,help(grid,i+1,j1+k,j2+l,dp));
+   int res=j1==j2?grid[i][j1]:grid[i][j1]+grid[i][j2];
+   return dp[i][j1][j2]=res+ans;
 }
     int cherryPickup(vector<vector<int>>& grid) {
-        int n=grid[0].size();
-        int m=grid.size();
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
-        return help(grid,0,0,n-1,dp);
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        return help(grid,0,0,grid[0].size()-1,dp);
     }
 };
