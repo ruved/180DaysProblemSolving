fb4d5f75270cfa1b578e85a83826dba10716f159
Ruved Aalam <ruvedalam7776@gmail.com>
2026-04-07 16:55:25 +0530

    Time: 21 ms (72.92%) | Memory: 37.8 MB (56.57%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-               int moveX[4] = {0, 1, 0, -1};
-        int moveY[4] = {1, 0, -1, 0};
-        // north 0, east 1, south 2, west 4
-        set<pair<int, int>> st;
-        for (auto it : obstacles) {
-            st.insert({it[0], it[1]});
-        }
-        int x_start = 0, y_start = 0, direction = 0;
-        int maxi = 0;
-        for (auto it : commands) {
-            // turn west
-            if (it == -2) {
-                direction = (direction + 3) % 4;
-            }
-            // turn east
-            if (it == -1) {
-                direction = (direction + 1) % 4;
-            }
-            // move required steps
-            else {
-                int moves = it;
-                while (moves > 0) {
-                    int x_new = x_start + moveX[direction];
-                    int y_new = y_start + moveY[direction];
+        int x = 0, y = 0;
 
-                    if (st.find({x_new, y_new}) == st.end()) {
-                        x_start = x_new;
-                        y_start = y_new;
-                        moves--;
-                        maxi = max(maxi, ((x_start*x_start)+(y_start*y_start)));
-                    }
-                    else{
-                        break;
-                    }
+        vector<vector<int>> dir = {{0,1,0,-1},{1,0,-1,0}}; // N,E,S,W
+        int pos = 0;
+
+        set<pair<int,int>> st;
+        for(auto &o : obstacles)
+            st.insert({o[0], o[1]});
+        int ans=0;
+        for(int cmd : commands){
+            if(cmd == -2){
+                pos = (pos + 3) % 4; // left
+            }else if(cmd == -1){
+                pos = (pos + 1) % 4; // right
+            }else{
+                while(cmd--){
+                    int nx = x + dir[0][pos];
+                    int ny = y + dir[1][pos];
+
+                    if(st.count({nx, ny})) break;
+
+                    x = nx;
+                    y = ny;
                 }
             }
+            ans=max(ans,x*x + y*y);
         }
-        return maxi;
 
+        return ans;
     }
 };
