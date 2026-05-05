637f02fc9ce70f674959862c636d853586b89761
Ruved Aalam <ruvedalam7776@gmail.com>
2026-05-05 23:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (20.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,8 +6,17 @@ int help(vector<int>& nums,int i,vector<int>&dp){
     return dp[i]=max(nums[i]+help(nums,i-2,dp),help(nums,i-1,dp));
 }
     int rob(vector<int>& nums) {
-        int n=nums.size()-1;
-        vector<int>dp(n+1,-1);
-        return help(nums,n,dp);
+        int n=nums.size();
+        vector<int>dp(n,0);
+        // return help(nums,n,dp);
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<n;++i)
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        
+        return dp[n-1];
     }
 };
