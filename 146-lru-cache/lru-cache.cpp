e0fc942f1df9729d2d65917082cc02ce723c770e
Ruved Aalam <ruvedalam7776@gmail.com>
2026-09-02 18:59:04 +0530

    Time: 16 ms (89.79%) | Memory: 47.8 MB (97.78%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,55 +1,119 @@
+class Node {
+public:
+    int key;
+    int value;
+
+    Node* prev;
+    Node* next;
+
+    Node(int key, int value) {
+        this->key = key;
+        this->value = value;
+        prev = nullptr;
+        next = nullptr;
+    }
+    
+};
+
 class LRUCache {
 public:
-  // key -> {value, latest timestamp}
-    unordered_map<int, pair<int, int>> mp;
+    unordered_map<int,Node*>mp;
+    int capacity;
+    Node *head;
+    Node *tail;
+    // Remove a node from the linked list
+    void remove(Node* node) {
 
-    // {key, timestamp}
-    queue<pair<int, int>> q;
+        Node* prevNode = node->prev;
+        Node* nextNode = node->next;
 
-    int timer = 0;
-    int cap;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+    }
 
+
+    // Insert node just before tail
+    void insert(Node* node) {
+
+        Node* prevNode = tail->prev;
+
+        prevNode->next = node;
+        node->prev = prevNode;
+
+        node->next = tail;
+        tail->prev = node;
+    }
     LRUCache(int capacity) {
-        cap = capacity;
-    }
+       this->capacity = capacity;
 
+        // Dummy nodes
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        head->next = tail;
+        tail->prev = head;
+    }
+    
     int get(int key) {
-        if (mp.find(key) == mp.end())
+
+        // Key doesn't exist
+        if (mp.find(key) == mp.end()) {
             return -1;
+        }
 
-        timer++;
+        Node* node = mp[key];
 
-        // Update latest timestamp
-        mp[key].second = timer;
+        // This key was just used,
+        // so it becomes MRU.
 
-        // Record this latest access
-        q.push({key, timer});
+        remove(node);
+        insert(node);
 
-        return mp[key].first;
+        return node->value;
     }
+
 
     void put(int key, int value) {
-        timer++;
 
-        // Update/insert key
-        mp[key] = {value, timer};
+        // CASE 1: key already exists
+        if (mp.find(key) != mp.end()) {
 
-        // Record latest access
-        q.push({key, timer});
+            Node* node = mp[key];
 
-        // Cache has exceeded capacity
-        while (mp.size() > cap) {
+            // Update value
+            node->value = value;
 
-            auto [oldKey, oldTime] = q.front();
-            q.pop();
+            // Move to MRU
+            remove(node);
+            insert(node);
 
-            // Check whether this is still the latest
-            // access of oldKey
-            if (mp.find(oldKey) != mp.end() &&
-                mp[oldKey].second == oldTime) {
+            return;
+        }
 
-                mp.erase(oldKey);
-            }
+
+        // CASE 2: new key
+
+        Node* newNode = new Node(key, value);
+
+        mp[key] = newNode;
+
+        insert(newNode);
+
+
+        // Capacity exceeded
+        if (mp.size() > capacity) {
+
+            // First real node after head
+            Node* lru = head->next;
+
+            // Remove from map
+            mp.erase(lru->key);
+
+            // Remove from linked list
+            remove(lru);
+
+            // Free memory
+            delete lru;
         }
     }
 };
