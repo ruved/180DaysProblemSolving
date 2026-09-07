13c6761e461e013331ee76da67a3fb74ba8cc95c
Ruved Aalam <ruvedalam7776@gmail.com>
2026-09-07 21:15:19 +0530

    Time: 4 ms (66.58%) | Memory: 24 MB (32.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,21 +3,15 @@ public:
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        stack<vector<int>>st;
-        st.push(intervals[0]);
-        for(int i=1;i<intervals.size();++i){
-            vector<int>last=st.top();
-            if(intervals[i][0]<=last[1]){
-                st.pop();
-                st.push({min(last[0],intervals[i][0]),max(last[1],intervals[i][1])});
-            }else{
-                st.push(intervals[i]);
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= res.back()[1]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
             }
-        }
-        vector<vector<int>>res;
-        while(!st.empty()){
-            res.push_back(st.top());
-            st.pop();
         }
         return res;
     }
