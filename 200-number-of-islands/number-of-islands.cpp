ad0476e5421d5928028e735afdb1836ca4de0dc6
Ruved Aalam <ruvedalam7776@gmail.com>
2026-09-08 20:29:43 +0530

    Time: 24 ms (77.23%) | Memory: 16.8 MB (60.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-void help(vector<vector<char>>& grid,int i,int j,vector<vector<bool>>&vis){
-    if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||vis[i][j]||grid[i][j]=='0') return ;
+bool checkboundry(vector<vector<char>>& grid,int i,int j){
+    if(i<0||j<0) return true;
+    if((i>=grid.size())||(j>=grid[0].size())) return true;
+    return false;
+}
+void help(vector<vector<char>>& grid,int i,int j,vector<vector<bool>>& vis){
+    if(checkboundry(grid,i,j)) return;
+    if((vis[i][j])||(grid[i][j]=='0')) return ;
     vis[i][j]=true;
     help(grid,i-1,j,vis);
     help(grid,i+1,j,vis);
-    help(grid,i,j+1,vis);
     help(grid,i,j-1,vis);
+    help(grid,i,j+1,vis);
 }
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
-        int ans=0;
-        for(int i=0;i<m;++i)
-            for(int j=0;j<n;++j)
-                if(!vis[i][j]&&grid[i][j]=='1'){
+        int cnt=0;
+        vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
+        for(int i=0;i<grid.size();++i){
+            for(int j=0;j<grid[0].size();++j){
+                if(!vis[i][j]&&(grid[i][j]=='1')){
                     help(grid,i,j,vis);
-                    ans++;
+                    cnt++;
                 }
-        return ans;
+            }
+        }
+        return cnt;
+        
     }
 };
-  
